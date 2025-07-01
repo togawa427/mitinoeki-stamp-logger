1d5b9f0890c8bb9113d30b8227a4ccd59c276c94
togawa427 <95838999+togawa427@users.noreply.github.com>
2025-07-01 13:12:49 +0900

    feat: tailwindcssを使えるようにした

---
+++ src/app/components/Base.tsx
@@ -0,0 +1,12 @@
+// import { Button } from '@mantine/core'
+import React from 'react'
+
+function Base() {
+  return (
+    <div>
+      aaaaa
+    </div>
+  )
+}
+
+export default Base
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full text-white bg-slate-700 text-2xl py-2 pl-2'>
+      スタンプラリー
+    </div>
+  )
+}
